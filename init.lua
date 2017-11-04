d1d93f76495e2eda19beddcb20baa08acd19866f
Matthias Berla <matthias@berla.net>
2017-11-04 19:29:03 +0100

    Remove module syntax as required by lua 5.3

--- init.lua
+++ init.lua
@@ -2,29 +2,29 @@ local os = os
 local client = client
 local pairs = pairs
 
-module("awesome-fullscreen")
+local _M = {}
 
-local settings = {
+_M.settings = {
 	callback = function(screen, ontop) end
 }
 
 -- {{{ Make fullscreen clients ontop
-local fullscreenwindows = {}
+_M.fullscreenwindows = {}
 
 client.connect_signal("unmanage", function(c) 
-	fullscreenwindows[c.window] = nil
+	_M.fullscreenwindows[c.window] = nil
 end)
 
 client.connect_signal("property::fullscreen", function(c)
-settings.callback(c.screen, not c.fullscreen)
+_M.settings.callback(c.screen, not c.fullscreen)
 if c.fullscreen == true then 
-	fullscreenwindows[c.window] = true
+	_M.fullscreenwindows[c.window] = true
 else
-	fullscreenwindows[c.window] = nil
+	_M.fullscreenwindows[c.window] = nil
 end
 local somewindowinfullscreen = false 
 -- Toggle power save functionality and blanking
-for _, w in pairs(fullscreenwindows) do 
+for _, w in pairs(_M.fullscreenwindows) do 
 	if w == true then
 		somewindowinfullscreen = true	
 		break	
@@ -39,4 +39,4 @@ else
 end
 end)
 -- }}}
-return settings
+return _M.settings
